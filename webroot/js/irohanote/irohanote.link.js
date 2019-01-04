0db1ad51d0c8145654b7bec36b9b0c0b59196849
Kotaro Miura <miura@irohasoft.jp>
2019-01-04 18:58:23 +0900

    Add copyright header in js and css files

--- webroot/js/irohanote/irohanote.link.js
+++ webroot/js/irohanote/irohanote.link.js
@@ -1,3 +1,12 @@
+/**
+ * iroha Compass Project
+ *
+ * @author        Kotaro Miura
+ * @copyright     2015-2018 iroha Soft, Inc. (http://irohasoft.jp)
+ * @link          http://irohacompass.irohasoft.jp
+ * @license       http://www.gnu.org/licenses/gpl-3.0.en.html GPL License
+ */
+ 
 // -------------------------------------------------
 // リンクマネージャクラス
 // -------------------------------------------------
