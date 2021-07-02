e5305259e34f006ab3cbfebcf1f99b0c4d904619
Kotaro Miura <miura@irohasoft.jp>
2021-07-02 18:22:06 +0900

    不要なルーティングを削除

--- Config/routes.php
+++ Config/routes.php
@@ -21,7 +21,7 @@
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
-	Router::connect('/pages/*', ['controller' => 'pages', 'action' => 'display']);
+//	Router::connect('/pages/*', ['controller' => 'pages', 'action' => 'display']);
 
 /**
  * Load all plugin routes. See the CakePlugin documentation on
