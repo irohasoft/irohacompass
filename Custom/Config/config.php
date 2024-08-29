9e7889e69a1492f72e725b476ba4c2c71bdab21c
Kotaro Miura <miura@irohasoft.jp>
2024-08-29 15:55:04 +0900

    不要なコードを削除

--- Custom/Config/config.php
+++ Custom/Config/config.php
@@ -11,31 +11,5 @@
 // Custom Config
 $config['dummy']		= [];
 
-// 受講者ログイン画面に管理者ログイン用リンクを表示する場合は以下のコメントを解除してください。
-//$config['show_admin_link'] = true;
 
-// お知らせのリンクを同じ画面で開く場合は以下のコメントを解除してください。
-//$config['open_link_same_window'] = true;
 
-// 感情アイコン
-//$config['use_emotion_icon']	= true;
-
-// 詳細なログ記録機能を使用する
-//$config['use_detailed_logging']	= true;
-
-// 感情アイコンを使用する
-//$config['use_emotion_icon']	= true;
-
-// スマイル機能を使用する
-//$config['use_smile']		= true;
-
-// iroha Note を使用する
-//$config['use_irohanote']	= true;
-
-/*
-$config['content_type']	= array(
-	'text'		=> 'テキスト',
-	'markdown'	=> 'Markdown',
-	'irohanote'	=> 'iroha Note',
-);
-*/
